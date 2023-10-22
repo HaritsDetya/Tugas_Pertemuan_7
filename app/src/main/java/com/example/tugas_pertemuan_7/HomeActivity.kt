e7fc9830fab5a54cefdf7d234792b08ed0b89851
Harits <haritsirawan2118@gmail.com>
2023-10-22 20:29:43 +0700

    update

--- app/src/main/java/com/example/tugas_pertemuan_7/HomeActivity.kt
+++ app/src/main/java/com/example/tugas_pertemuan_7/HomeActivity.kt
@@ -1,8 +1,13 @@
 package com.example.tugas_pertemuan_7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.tugas_pertemuan_7.databinding.ActivityMainBinding
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.tugas_pertemuan_7.LoginFragment.Companion.EXTRA_NAME
+import com.example.tugas_pertemuan_7.RegisterFragment.Companion.EXTRA_NAME1
 import com.example.tugas_pertemuan_7.databinding.HomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -11,7 +16,27 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = HomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        with(binding) {
+        val username = intent.getStringExtra(EXTRA_NAME)
+        val username1 = intent.getStringExtra(EXTRA_NAME1)
+        with(binding){
+            name1.text = username1
+            name.text = username
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.dashboard, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.logout -> {
+                Toast.makeText(this@HomeActivity, "Logout", Toast.LENGTH_SHORT).show()
+                val intentHomeActivity =
+                    Intent(this, MainActivity::class.java)
+                startActivity(intentHomeActivity)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
--- app/src/main/java/com/example/tugas_pertemuan_7/RegisterFragment.kt
+++ app/src/main/java/com/example/tugas_pertemuan_7/RegisterFragment.kt
@@ -3,10 +3,12 @@ package com.example.tugas_pertemuan_7
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import com.example.tugas_pertemuan_7.databinding.FragmentRegisterBinding
 import com.google.android.material.textfield.TextInputEditText
@@ -15,9 +17,9 @@ class RegisterFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
 
     companion object{
-        const val EXTRA_NAME = "extra_name"
-        const val EXTRA_EMAIL = "extra_email"
-        const val EXTRA_PASS = "extra_pass"
+        const val EXTRA_NAME1 = "extra_name"
+        const val EXTRA_EMAIL1 = "extra_email"
+        const val EXTRA_PASS1 = "extra_pass"
     }
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
@@ -34,9 +36,9 @@ class RegisterFragment : Fragment() {
         register.setOnClickListener {
             val intentToRegisterFragment =
                 Intent(requireContext(), HomeActivity::class.java)
-            intentToRegisterFragment.putExtra(EXTRA_NAME, usernameInput.text.toString())
-            intentToRegisterFragment.putExtra(EXTRA_EMAIL, emailInput.text.toString())
-            intentToRegisterFragment.putExtra(EXTRA_PASS, passwordInput.text.toString())
+            intentToRegisterFragment.putExtra(EXTRA_NAME1, usernameInput.text.toString())
+            intentToRegisterFragment.putExtra(EXTRA_EMAIL1, emailInput.text.toString())
+            intentToRegisterFragment.putExtra(EXTRA_PASS1, passwordInput.text.toString())
             startActivity(intentToRegisterFragment)
         }
         return view
